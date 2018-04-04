78abd509bfe2aa460762a1ca0b8654a63c1b8f5a
Evan Candler <emcandler21@gmail.com>
2018-04-04 19:46:05 +0000

    automatically backed up by learn

--- artist.rb
+++ artist.rb
@@ -1,11 +1,2 @@
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
+class Artist < ActiveRecord::Base
+end
